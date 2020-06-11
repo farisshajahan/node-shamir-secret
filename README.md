a4e9e469d413080cfd675561081c5049a3ff768d
Faris Shajahan <farisshajahan@protonmail.com>
2020-06-11 10:54:23 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# RSA Shamir Secret Sharing over SMTP
+
+### Steps to run
+
+1. `git clone https://github.com/farisshajahan/node-shamir-secret`
+2. `cd node-shamir-secret`
+3. `npm install`
+4. `mv config.json.example config.json`
+5. Edit `config.json` with your configurations
+6. `node index.js`
---
+++ index.js
@@ -0,0 +1,55 @@
+const crypto = require('crypto')
+const { split, join } = require('shamir')
+const nodemailer = require('nodemailer')
+const config = require('./config.json')
+
+async function sendmail(email, content) {
+    let transporter = nodemailer.createTransport({
+        host: config.host,
+        port: config.port,
+        secure: false,
+        auth: {
+            user: config.from,
+            pass: config.password
+        }
+    });
+
+    let info = await transporter.sendMail({
+        from: config.fromtext,
+        to: email,
+        subject: "Private Key Share",
+        text: "Your share(s): "+content,
+        html: "<h2>Your share(s): "+content+"</h2>"
+    });
+}
+
+function generateKeys() {
+    return crypto.generateKeyPairSync('rsa', {
+        modulusLength: 2048,
+        publicKeyEncoding: {
+            type: 'spki',
+            format: 'pem'
+        },
+        privateKeyEncoding: {
+            type: 'pkcs8',
+            format: 'pem'
+        }
+    });
+}
+
+function shamirSplit(secret, parts) {
+    const utf8Encoder = new TextEncoder();
+    const secretBytes = utf8Encoder.encode(secret);
+    return split(crypto.randomBytes, parts, parts, secretBytes);
+}
+
+function main() {
+    const { privateKey, publicKey } = generateKeys();
+    let parts = shamirSplit(privateKey, config.emails.length);
+    var count = 0;
+    for (sharenumber in parts) {
+        sendmail(config.emails[count++], "("+sharenumber+", "+Buffer.from(parts[sharenumber]).toString('hex')+")");
+    }
+}
+
+main()
